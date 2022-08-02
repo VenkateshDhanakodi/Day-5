208aa402aaa1e88e6e402947d191b80e320401c8
VenkateshDhanakodi <110087599+VenkateshDhanakodi@users.noreply.github.com>
2022-08-02 19:13:01 +0530

    Add files via upload

---
+++ Anonymous function & IIFE/remove_dup.js
@@ -0,0 +1,18 @@
+	let arr = ["venkat", "kutty", "sathya",5,
+			"I_am", "I_want", "sathya",5,7,5,5,7,7,"sathya"];
+	
+			let Dup = (function(a) {
+		 		return (a.filter((item, index) =>
+            	a.indexOf(item) === index));
+	})(arr);
+	let final = Dup;
+	console.log(final);
+
+		// let arr = ["venkat", "kutty", "sathya",5,
+	// 		"I_am", "I_want", "sathya",5,7];
+
+	// function Dup(arr) {
+	// 	return arr.filter((item, index) =>
+    //         arr.indexOf(item) === index);
+	// }
+	// console.log(Dup(arr));
